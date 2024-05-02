bdba5c3c150bd27039f0ce5793b1dfb1730edfac
Bruno de Freitas <46200462+brunofreitasv@users.noreply.github.com>
2024-05-02 14:23:28 -0300

    some changes

--- src/components/Hacker/Hacker.js
+++ src/components/Hacker/Hacker.js
@@ -3,7 +3,7 @@ import './Hacker.css';
 export const  Hacker = (props) => {
   return (
     <div className='hacker'>
-        <div className='header'>
+        <div className='header' style={{ backgroundColor: props.backgroundColor }}>
             <img src={props.picture} alt={props.nickname} />
         </div>
         <div className='baseboard'>
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -8,7 +8,7 @@ export const  Team = (props) => {
             <h3 style={{borderColor: props.primaryColor}}>{props.name}</h3>
             <div className='hackers'>
                 {
-                    props.hackers.map(h => <Hacker key={h.name} name={h.name} nickname={h.nickname} mantra={h.mantra} picture={h.picture} />)
+                    props.hackers.map(h => <Hacker key={h.name} name={h.name} nickname={h.nickname} mantra={h.mantra} picture={h.picture} backgroundColor={props.primaryColor} />)
                 }
             </div>
         </section>  
